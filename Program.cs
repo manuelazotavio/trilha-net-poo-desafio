9f72f855b1ea8cfc3d52744dcc2e867e4fa1c69e
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-09-30 14:58:07 -0300

    feat: resolvendo desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
+using System;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nokia nokia = new Nokia("11942881397", "Nokia Preto", "3213213", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Instagram");
+
+        Iphone iphone = new Iphone("4820020399", "Iphone 11 Pro", "3829323", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Github");
+    }
+}
